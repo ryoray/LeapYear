735e549bc16bc47a4e2f0eba90e4d0ede88d83f5
ryoray <praretno.wibowo@gmail.com>
2020-04-16 13:25:19 +0700

    Print the result on class Main

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,13 +1,5 @@
 public class LeapYear {
 
-    public static void main(String[] args) {
-
-        System.out.println(isLeapYear(-1600));
-        System.out.println(isLeapYear(1600));
-        System.out.println(isLeapYear(2017));
-        System.out.println(isLeapYear(2000));
-    }
-
     public static boolean isLeapYear (int year) {
         boolean result;
         if ((year <= 1) || (year >= 9999)) {
